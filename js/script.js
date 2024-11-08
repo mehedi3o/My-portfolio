0bc4aa80286a0968699b915a272215c1c763965e
mehedip1 <Promehedi30@gmail.com>
2024-11-08 16:00:21 +0600

    my link file also add

--- js/script.js
+++ js/script.js
@@ -25,4 +25,25 @@ function colsemenu(){
 }
 
 
+// gooogle sheet---
+
+const apiUrl = 'https://script.google.com/macros/s/AKfycbxD-fWLOfSR6_kmvsBhbD4FmBJvr-zlyagLoaR3VNzl5K67ytZGsPs8RmOF34OMDPQgbQ/exec';
+const submissionForm = document.forms['data-to-google-sheet'];
+
+submissionForm.addEventListener('submit', event => {
+  event.preventDefault();
+//   fetch(apiUrl, { method: 'POST', body: new FormData(submissionForm) })
+//     .then(response => console.log('Submission successful!', response))
+//     .catch(error => console.error('Submission error!', error.message));
+
+
+fetch(apiUrl, { method: 'POST', body: new FormData(submissionForm) })
+  .then(response => response.json())
+  .then(data => console.log('Submission successful!', data))
+//   .catch(error => console.error('Submission error!', error.message));
+});
+            
+
+
+
 
